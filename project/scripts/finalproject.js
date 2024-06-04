659c1b4942dcd2c0a09068d3d704c2486619131f
0laf3 <capindissa@hotmail.com>
2024-06-04 12:18:53 +0100

    change syntax

--- project/scripts/finalproject.js
+++ project/scripts/finalproject.js
@@ -1,33 +1,33 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2838e19881854431b204e47ac0d0d9b8&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2838e19881854431b204e47ac0d0d9b8&page=1'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=2838e19881854431b204e47ac0d0d9b8&query="'
 
 const main = document.getElementById('main')
-const from = document.getElementById('form')
+const form = document.getElementById('form')
 const search = document.getElementById('search')
 
 //get initial movies 
 getMovies(API_URL)
 
-async function getMovies(url){
+async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json
 
     showMovies(data.result)
 }
 
-function showMovies(movies){
+function showMovies(movies) {
     MediaDeviceInfo.innerHTML = ''
 
-    movies.forEach((movie) =>{
-        const{ title, poster_path, vote_average, 
-            overview} = movie
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average,
+            overview } = movie
 
         const movieEl = document.createElement
-        ('div')
+            ('div')
         movieEl.classList.add(movie)
 
-        movieEl.innerHTML=`
+        movieEl.innerHTML = `
         <img src="${IMG_PATH + poster_path}" alt="${title}">
         <div class="movie-info">
             <h3>${title}</h3>
@@ -38,30 +38,30 @@ function showMovies(movies){
             ${overview}
         </div>
     `
-    main.appendChild(movieEl)
+        main.appendChild(movieEl)
     })
 }
 
-function getClassByRate(vote){
-    if(vote >= 8){
+function getClassByRate(vote) {
+    if (vote >= 8) {
         return 'green'
-    } else if(vote >= 5){
+    } else if (vote >= 5) {
         return 'orange'
     } else {
         return 'red'
     }
 }
 
-from.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const searchTerm = search.value
 
-    if (searchTerm && searchTerm !== ''){
+    if (searchTerm && searchTerm !== '') {
         getMovies(SEARCH_API + searchTerm)
 
         search.value = ''
-    } else{
+    } else {
         Window.location.reload()
     }
 })
