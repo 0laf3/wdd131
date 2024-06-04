9fd7a763549f591098cd174c90b04a69dbd61354
0laf3 <capindissa@hotmail.com>
2024-06-04 12:02:59 +0100

    api key

--- project/scripts/finalproject.js
+++ project/scripts/finalproject.js
@@ -1,6 +1,6 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2838e19881854431b204e47ac0d0d9b8&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const SEARCH_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2838e19881854431b204e47ac0d0d9b8&page=1'
 
 const main = document.getElementById('main')
 const from = document.getElementById('form')
