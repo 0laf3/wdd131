07f0a06e8fe0ea8cbefe9ac323b17cb648f252e9
João Sebastião Capindissa <43992006+0laf3@users.noreply.github.com>
2024-10-29 15:32:01 +0100

    Update finalproject.js
    menu

--- project/scripts/finalproject.js
+++ project/scripts/finalproject.js
@@ -79,4 +79,11 @@ function myFunction() {
         x.style.display = "block";
     }
 }
-
+function myFunction(menuId) {
+    var menu = document.getElementById(menuId);
+    if (menu.style.display === "flex") {
+        menu.style.display = "none"; // Hide the menu
+    } else {
+        menu.style.display = "flex"; // Show the menu
+    }
+}
